c78ed13bf699ef2d7e5ec5d2f26f281a4275ffdd
oktaw <oktaw@DESKTOP-6DTAQ2J>
2022-01-22 17:03:40 +0100

    Initial commit

---
+++ app/src/main/java/com/example/niezapominajka/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.niezapominajka
+
+import android.content.Context
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.PopupWindow
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //val buttonPopup = document.findViewById<Button>(R.id.button)
+
+        final Button button = findViewById(R.id.button);
+    }
+}
